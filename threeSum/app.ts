7b582ddd3e9564c53bebfc71af3e7943f041846a
unknown <ramovic225@gmail.com>
2025-02-15 14:20:51 +0100

    threeSum

---
+++ threeSum/app.ts
@@ -0,0 +1,25 @@
+function threeSum(nums: number[]): number[][] {
+  let arr: number[][] = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = i + 2; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0)
+          arr.push([nums[i], nums[j], nums[k]]);
+      }
+    }
+  }
+
+  let finArray: number[][] = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].sort();
+    if (!JSON.stringify(finArray).includes(JSON.stringify(arr[i])))
+      finArray.push(arr[i]);
+  }
+
+  return finArray;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([0, 1, 1]));
+console.log(threeSum([0, 0, 0]));
